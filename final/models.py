73d7b67fdd6a8b47e12d8dfb13ae4637aee92c75
Esther Ko <ck3023@columbia.edu>
2020-10-12 02:40:55 +0000

    add final app and location model

---
+++ final/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+
+class Location(models.Model):
+    Latitude = models.FloatField()
+    Longtitude = models.FloatField()
+    Unique_Squirrel_ID = models.CharField(max_length = 100)
+    
+    AM = 'AM'
+    PM = 'PM'
+    OTHER = ''
+    shift_choice = [
+            (AM, 'AM'),
+            (PM, 'PM'),
+            (OTHER, '')
+            ]
+    Shift = models.CharField(
+            max_length = 15,
+            choices = shift_choice,
+            default = OTHER,
+            )
+    
+    Date = models.CharField(max_length = 100,default = '')
+    
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+    age_choice = [
+            (ADULT,'Adult'),
+            (JUVENILE,'Juvenile'),
+            (OTHER,''),
+            ]
+    Age = models.CharField(
+        max_length=30,
+        choices=age_choice,
+        default=OTHER,
+        blank=True,
+    )
+
+    GRAY = 'Gray'
+    CINNAMON = 'Cinnamon'
+    BLACK = 'Black'
+    fur_choice = [
+        (GRAY, 'Gray'),
+        (CINNAMON, 'Cinnamon'),
+        (BLACK, 'Black'),
+        (OTHER, ''),
+    ]
+
+    Primary_Fur = models.CharField(
+        max_length=30,
+        choices=fur_choice,
+        default=OTHER,
+        blank=True,
+    )
+
+    GROUND = 'Ground'
+    ABOVE = 'Above'
+    location_choice = [
+        (GROUND, 'Ground'),
+        (ABOVE, 'Above'),
+        (OTHER, ''),
+    ]
+
+    Location = models.CharField(
+        max_length=30,
+        choices=location_choice,
+        default=OTHER,
+        blank=True,
+    )
+
+    Specific_Location = models.CharField(max_length=100, blank=True)
+    Running = models.BooleanField()
+    Chasing = models.BooleanField()
+    Climbing = models.BooleanField()
+    Eating = models.BooleanField()
+    Foraging = models.BooleanField()
+
+    Other_Activities = models.CharField(max_length=100, blank=True)
+    Kuks = models.BooleanField()
+    Quaas = models.BooleanField()
+    Moans = models.BooleanField()
+    Tail_Flags = models.BooleanField()
+    Tail_Twitches = models.BooleanField()
+    Approaches = models.BooleanField()
+    Indifferent = models.BooleanField()
+    Runs_From = models.BooleanField()
--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,5 @@
-"""project URL Configuration
+"""
+project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('final.urls')),
 ]
