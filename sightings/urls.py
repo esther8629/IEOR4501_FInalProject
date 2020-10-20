a8714c102ef5e748d9e6cd03b33f96d3fd82325e
Beryl Wu <hw2798@columbia.edu>
2020-10-20 05:53:22 +0000

    Add navbar to /add

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'sightings'
 
 urlpatterns = [
-        path('',views.show),
+        path('',views.show, name='show'),
         path('add/',views.add_sightings, name = 'add'),
         path('stats/', views.stats, name = 'stats'),
         path('<Unique_Squirrel_ID>/',views.detail,name = 'detail'),
